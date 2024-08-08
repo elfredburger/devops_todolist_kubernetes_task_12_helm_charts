770d1cacb6b1ec2e1875b5882648feccea27991a
elfredburger <elfredfredburger@gmail.com>
2024-08-08 03:42:42 +0200

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kind create cluster --config ./cluster.yml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-helm install todoapp ./.infrastructure/todoapp/
+helm install todoapp ./.infrastructure/helm-chart/todoapp/
 
